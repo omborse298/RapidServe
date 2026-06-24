9e6f57d4c09a38b8021d18e6423158121fe33034
Om Borse <borseom2008@gmail.com>
2026-06-24 13:43:59 +0000

    Added splash screen and navigation setup

---
+++ lib/utils/app_routes.dart
@@ -0,0 +1,10 @@
+class AppRoutes {
+  static const splash = "/";
+  static const login = "/login";
+  static const register = "/register";
+  static const forgotPassword = "/forgot-password";
+
+  static const customerHome = "/customer-home";
+  static const providerDashboard = "/provider-dashboard";
+  static const adminDashboard = "/admin-dashboard";
+}
