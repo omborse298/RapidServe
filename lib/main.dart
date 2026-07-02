22e2cb8d4b7a8b35e8ce4f29878f345fd3bbe06d
Om Borse <borseom2008@gmail.com>
2026-07-02 10:14:19 +0000

    Splash screen done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'screens/splash_screen.dart';
 
 void main() {
   runApp(const RapidServeApp());
@@ -9,23 +10,9 @@ class RapidServeApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("RapidServe"),
-          centerTitle: true,
-        ),
-        body: const Center(
-          child: Text(
-            "Welcome to RapidServe",
-            style: TextStyle(
-              fontSize: 28,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ),
-      ),
+      home: SplashScreen(),
     );
   }
 }
